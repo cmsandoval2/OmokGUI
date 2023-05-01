43bc5e4395b3ca51a217e55ecf07b134d1e1b758
Carlos <cmsandoval2@miners.utep.edu>
2023-05-01 13:49:42 -0600

    I believe that I have made the Client class recieve messages of moves from the Server

--- OmokGUI/src/omok/Client.java
+++ OmokGUI/src/omok/Client.java
@@ -6,6 +6,8 @@ import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 
+import javax.swing.JOptionPane;
+
 public class Client {
     private String host;
     private int port;
@@ -13,16 +15,46 @@ public class Client {
     private BufferedReader reader;
     private PrintWriter writer;
     private boolean running = false;
-
+    private Board board;
+    private Omok omok;
     public Client(String host, int port) {
         this.host = host;
         this.port = port;
+        omok = new Omok();
+        board = new Board(omok);
+
     }
     NetworkAdapter.MessageListener messageListener = new NetworkAdapter.MessageListener() {
         @Override
         public void messageReceived(NetworkAdapter.MessageType type, int x, int y) {
-            // Handle the received message
-        }
+        	 // Handle the received message
+            switch (type) {
+                case MOVE:
+                    // Update the client's board with the received move
+                    omok.handleMove(x, y);
+
+                    // Repaint the game board
+                    board.repaint();
+
+                    // Check for win condition
+                    if (omok.checkWin(x, y)) {
+                        // Display win message
+                        JOptionPane.showMessageDialog(null, "Opponent wins!");
+                        omok.gameEnded = true;
+                    } else if (omok.checkDraw()) {
+                        // Display draw message
+                        JOptionPane.showMessageDialog(null, "Draw!");
+                        omok.gameEnded = true;
+                    } else {
+                        // Switch to the current player's turn
+                        omok.switchTurn();
+                    }
+                    break;
+                default:
+                    // Handle other message types (if any)
+                    break;
+            }
+                    }
     };
     public void connect() {
     	try {
